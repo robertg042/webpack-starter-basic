98e26bbdc313f5779e67e4d712b3bbc4c1ba0e54
Robert Górnicki <github@gorki.net.pl>
2018-06-01 13:24:53 +0200

    lato and font-awesome fonts added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import "normalize.css/normalize.css";
+import "sanitize.css/sanitize.css";
+import "./assets/fonts/font-awesome/fontawesome.css";
+import "./assets/fonts/lato/lato.css";
 import "./styles/index.scss";
 
 document.addEventListener("DOMContentLoaded", () => {});
